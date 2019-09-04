c924b85e9936daff0bfd7329e8f3b6fe1c395d8a
Szorti <krzysztof.flisik@gmail.com>
2019-09-04 22:58:20 +0200

    first succes in asset import (assimp)

--- Armadillo/Geometry.cpp
+++ Armadillo/Geometry.cpp
@@ -1,13 +1,51 @@
 #include "Geometry.h"
 #include "AEPrimitive.h"
+#include <fstream>
+#include <iostream>
+
+// Needed for Assimp import objects
+#include <assimp/Importer.hpp>
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 bool Geometry::Init()
 {
 	AEPrimitive plane(eAE_PrimitiveType_Plane);
 
+	Assimp::Importer importer;
+	const std::string objFile = "./Objects/TriangleWithoutIndices.gltf";
+	const aiScene* scene = NULL;
+
+	std::ifstream fin(objFile.c_str());
+	if (!fin.fail())
+	{
+		fin.close();
+		std::cout << "file exist!" << std::endl;
+	}
+	else {
+		std::cout << "No import!" << std::endl;
+		std::cin.get();
+		return NULL;
+	}
+
+	scene = importer.ReadFile(objFile.c_str(), aiProcessPreset_TargetRealtime_Quality);
+
+	if (!scene)
+	{
+		std::cout << importer.GetErrorString() << std::endl;
+		std::cin.get();
+		return NULL;
+	}
+
+	std::cout << "Number of imported meshes: " << scene->mNumMeshes << std::endl;
+
+	aiMesh* triangle = scene->mMeshes[0];
+	aiVector3D* vertices = triangle->mVertices;
+
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), plane.GetVertexes(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, triangle->mNumVertices * sizeof(aiVector3D), &vertices[0], GL_STATIC_DRAW);
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
